e1764591e18c08cf22551653da7db9482c0b8e92
Shantanu Singh <shsingh@broadinstitute.org>
2016-09-16 19:05:38 +0000

    Update manual metadata to call from the command line

--- files/ManualMetadata.py
+++ files/ManualMetadata.py
@@ -5,12 +5,15 @@ Created on Mon Aug 15 12:27:08 2016
 @author: bcimini
 """
 import pandas as pd
+import sys
+
+csv=sys.argv[1]
 
 def manualmetadata():
-    incsv=pd.read_csv(r'C:\Example\Example.csv')
-    manmet=open(r'C:\Example\ExampleMetadata.txt','w')
+    incsv=pd.read_csv(csv)
+    manmet=open(csv[:-4]+'batch.txt','w')
     print incsv.shape
     for i in range(incsv.shape[0]):
-        manmet.write('{"Metadata": "Metadata_Plate='+str(incsv.Metadata_Plate[i])+',Metadata_Well='+str(incsv.Metadata_Well[i])+',Metadata_Site='+str(incsv.Metadata_Site[i])+'"},\n')
+        manmet.write('{"Metadata": "Metadata_Plate='+str(incsv.Metadata_Plate[i])+',Metadata_Well='+str(incsv.Metadata_Well[i])+',Metadata_Site='+str(int(incsv.Metadata_Site[i]))+'"},\n')
     manmet.close()
 manualmetadata()
