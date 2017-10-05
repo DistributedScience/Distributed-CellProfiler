e1eaf95c66f357c46a2bf334529fef8b926d66c1
Ubuntu <ubuntu@ip-10-0-3-178.ec2.internal>
2017-10-05 16:03:08 +0000

    Mismatched metadata should be a warning, not a termination (pt2)

--- worker/cp-worker.py
+++ worker/cp-worker.py
@@ -112,8 +112,6 @@ def runCellProfiler(message):
 				watchtowerlogger=watchtower.CloudWatchLogHandler(log_group=LOG_GROUP_NAME, stream_name=message['Metadata'],create_log_group=False)
 				logger.addHandler(watchtowerlogger)
 				printandlog('Your specified output structure does not match the Metadata passed',logger)
-				logger.removeHandler(watchtowerlogger)
-				return 'INPUT_PROBLEM' 
 			else:
 				metadataID = string.replace(metadataID,eachMetadata.split('=')[0],eachMetadata.split('=')[1])
 	    else: #backwards compatability with 1.0.0 and/or no desire to structure output
