6fd53b897aa343108dee2884dfd8130bb2080e33
Beth Cimini <beth.cimini@gmail.com>
2020-11-10 09:52:49 -0500

    make directory for files to land in

--- worker/cp-worker.py
+++ worker/cp-worker.py
@@ -202,7 +202,7 @@ def runCellProfiler(message):
                     new_file_name = os.path.join(localIn,prefix_on_bucket)
                     printandlog(full_old_file_name+prefix_on_bucket+new_file_name, logger)
                     if not os.path.exists(os.path.split(new_file_name)[0]):
-                        os.path.makedirs(os.path.split(new_file_name)[0])
+                        os.makedirs(os.path.split(new_file_name)[0])
                     s3.meta.client.download_file(AWS_BUCKET,prefix_on_bucket,new_file_name)
                     count +=1
             printandlog('Downloaded '+str(count)+' files')
