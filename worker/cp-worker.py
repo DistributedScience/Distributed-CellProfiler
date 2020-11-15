a16d25cd3e364e327b73927b212892fd7ea60f01
Beth Cimini <beth.cimini@gmail.com>
2020-11-14 22:17:14 -0500

    download pipeline and CSV, never use S3FS

--- worker/cp-worker.py
+++ worker/cp-worker.py
@@ -184,6 +184,8 @@ def runCellProfiler(message):
             old_pipeline_name = os.path.join(DATA_ROOT,message['pipeline'])
             pipeline_on_bucket_name = old_pipeline_name.split(DATA_ROOT)[1][1:]
             new_pipeline_name = os.path.join(localIn,pipeline_on_bucket_name)
+            if os.path.exists(new_pipeline_name):
+                os.remove(new_pipeline_name)
             s3.meta.client.download_file(AWS_BUCKET,pipeline_on_bucket_name,new_pipeline_name)
             #download CSV
             old_csv_name = os.path.join(DATA_ROOT,message['data_file'])
